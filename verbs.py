d4542f8740aa8ffb78759d9e84c0ff96e078debf
Vinicius Gabriel <121624375+ViniGabriel209@users.noreply.github.com>
2023-01-13 16:03:16 -0300

    Add files via upload

---
+++ verbs.py
@@ -0,0 +1,22 @@
+verbs = [
+{'idverb' : 1, 	'verb' : 'beat',   'pastSimple' : 'beat',	 'pastParticiple' : 'beaten',  'meaning' : 'ganhar de algo'},
+{'idverb' : 2,  'verb' : 'become', 'pastSimple' : 'became',  'pastParticiple' : 'become',  'meaning' : 'transformar, vir a ser'},
+{'idverb' : 3,  'verb' : 'begin',  'pastSimple' : 'began',   'pastParticiple' : 'begun',   'meaning' : 'começar, iniciar'},
+{'idverb' : 4,  'verb' : 'bend',   'pastSimple' : 'bent',	 'pastParticiple' : 'bent',    'meaning' : 'curvar, dobrar, torcer'},
+{'idverb' : 5,  'verb' : 'bet',    'pastSimple' : 'bet',	 'pastParticiple' : 'bet',     'meaning' : 'apostar'},
+{'idverb' : 6,  'verb' : 'bite',   'pastSimple' : 'bit',	 'pastParticiple' : 'bitten',  'meaning' : 'morder, picar'},
+{'idverb' : 7,  'verb' : 'bleed',  'pastSimple' : 'bled',	 'pastParticiple' : 'bled',    'meaning' : 'sangrar, perder muito dinheiro'},
+{'idverb' : 8,  'verb' : 'blow',   'pastSimple' : 'blew',	 'pastParticiple' : 'blown',   'meaning' : 'soprar [há outros significados]'},
+{'idverb' : 9,  'verb' : 'break',  'pastSimple' : 'broke',   'pastParticiple' : 'broken',  'meaning' : 'quebrar, finalizar/interromper'},
+{'idverb' : 10,	'verb' : 'breed',  'pastSimple' : 'bred',	 'pastParticiple' : 'bred',    'meaning' : 'procriar, causar algo ruim a acontecer'},
+{'idverb' : 11,	'verb' : 'bring',  'pastSimple' : 'brought', 'pastParticiple' : 'brought', 'meaning' : 'trazer'},
+{'idverb' : 12,	'verb' : 'buy',	   'pastSimple' : 'bought',	 'pastParticiple' : 'bought',  'meaning' : 'comprar'},
+{'idverb' : 13,	'verb' : 'catch',  'pastSimple' : 'caught',	 'pastParticiple' : 'caught',  'meaning' : 'capturar, pegar'},
+{'idverb' : 14,	'verb' : 'choose', 'pastSimple' : 'chose',	 'pastParticiple' : 'chosen',  'meaning' : 'escolher'},
+{'idverb' : 15,	'verb' : 'come',   'pastSimple' : 'came',	 'pastParticiple' : 'come',	   'meaning' : 'vir, chegar'},
+{'idverb' : 16,	'verb' : 'cost',   'pastSimple' : 'cost',	 'pastParticiple' : 'cost',	   'meaning' : 'custar'},
+{'idverb' : 17,	'verb' : 'cut',	   'pastSimple' : 'cut',     'pastParticiple' : 'cut',	   'meaning' : 'cortar'},
+{'idverb' : 18,	'verb' : 'dig',	   'pastSimple' : 'dug',     'pastParticiple' : 'dug',	   'meaning' : 'cavar, escavar'},
+{'idverb' : 19,	'verb' : 'draw',   'pastSimple' : 'drew',	 'pastParticiple' : 'drawn',   'meaning' : 'desenhar, chamar atenção'},
+{'idverb' : 20,	'verb' : 'drink',  'pastSimple' : 'drank',	 'pastParticiple' : 'drunk',   'meaning' : 'beber'}
+]
