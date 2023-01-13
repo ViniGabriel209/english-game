93b9f718ff0a8890f1267031a7b6cd13468054f8
Vinicius Gabriel <121624375+ViniGabriel209@users.noreply.github.com>
2023-01-13 13:03:43 -0300

    Update pythonGame.py

--- pythonGame.py
+++ pythonGame.py
@@ -23,12 +23,14 @@ class bcolors:
 
 def verbsConjugation():
 
+    i = 0
+
     while True:
 
-        question = rd.randint(1, 2)
+        question = rd.randint(1, 5)
 
         a = rd.randint(0, len(verbs) - 1)
-        print(f'a: {a}')
+      
         verbDictionary = verbs[a]
         columns = ['base form', 'past simple', 'past participle', 'meaning']
         columnNumber = rd.randint(0, 3)
@@ -38,40 +40,108 @@ def verbsConjugation():
         pastParticiple = verbDictionary['partParticiple']
         meaning = verbDictionary['meaning']
 
-        # if question == 1:
-        print(f'What is the past simple and past participle of \'{verb.upper()}\'?')
-        ps = input('Past Simple:     ')
-        pt = input('Past Participle: ')
+        if question == 1:
+            print(f'{bcolors.ENDC}What is the {bcolors.UNDERLINE}past simple{bcolors.ENDC} and {bcolors.UNDERLINE}past participle{bcolors.ENDC} of \'{verb.upper()}\'?')
+            ps = input(f'{bcolors.ENDC}Past Simple:     ')
+            pt = input(f'{bcolors.ENDC}Past Participle: ')
 
-        rightPS = False
-        rightPT = False
+            rightPS = False
+            rightPT = False
 
-        if ps == pastSimple:
-            rightPS = True
-        if pt == pastParticiple:
-            rightPT = True
+            if ps == pastSimple:
+                rightPS = True
+            if pt == pastParticiple:
+                rightPT = True
 
-        if rightPS and rightPT:
-            print(f'{bcolors.OKGREEN}RIGHT!')
-            print('='*20)
-            print(f'gabarito: {pastSimple}')
-            print(f'resposta: {ps}')
-        else:
-            print(f'\033[91mWRONG!')
-            print(f'Past Simple:     {pastSimple}')
-            print(f'Past Participle: {pastParticiple}')
-            print('='*20)
+            if rightPS and rightPT:
+                print(f'{bcolors.OKGREEN}RIGHT!')
+                i += 1
+            else:
+                print(f'{bcolors.FAIL}WRONG!')
+                print(f'{bcolors.ENDC}Past Simple:     {bcolors.WARNING}{ pastSimple}')
+                print(f'{bcolors.ENDC}Past Participle: {bcolors.WARNING}{pastParticiple}')
+                i = 0
 
-        Continue = input('Do you want to continue? [y/n] ')
+        # NEXT TYPE OF QUESTION
 
-        if Continue == 'n':
-            break
+        if question == 2:
+            print(f'{bcolors.ENDC}What is the {bcolors.UNDERLINE}verb{bcolors.ENDC} which past participle is \'{pastParticiple.upper()}\'?')
+            v = input('Verb: ')
 
-        # if question == 2:
-        #     print(f'What is the verb that such past participle is \'{pastParticiple.upper()}\'?')
-        #     v = input('Verb: ')
+            rightV = False
 
-        #     rightV = False
+            if v == verb:
+                rightV = True
+
+            if rightV:
+                print(f'{bcolors.OKGREEN}RIGHT!')
+                print(f'{bcolors.ENDC}The meaning of {verb} is: {bcolors.WARNING}{meaning}')
+                i += 1
+            else:
+                print(f'{bcolors.FAIL}WRONG!')
+                print(f'{bcolors.ENDC}The verb is {verb}')
+                i = 0
+
+        if question == 3:
+            print(f'{bcolors.ENDC}What is the {bcolors.UNDERLINE}verb{bcolors.ENDC} which past simple is \'{pastSimple.upper()}\'?')
+            v = input('Verb: ')
+
+            rightV = False
+
+            if v == verb:
+                rightV = True
+
+            if rightV:
+                print(f'{bcolors.OKGREEN}RIGHT!')
+                print(f'{bcolors.ENDC}The meaning of {verb} is: {bcolors.WARNING}{meaning}')
+                i += 1
+            else:
+                print(f'{bcolors.FAIL}WRONG!')
+                print(f'{bcolors.ENDC}The verb is {verb}')
+                i = 0
+
+        if question == 4:
+            print(f'{bcolors.ENDC}What is the {bcolors.UNDERLINE}past simple form{bcolors.ENDC} which past participle is \'{pastParticiple.upper()}\'?')
+            v = input('Verb: ')
+
+            rightV = False
+
+            if v == pastSimple:
+                rightV = True
+
+            if rightV:
+                print(f'{bcolors.OKGREEN}RIGHT!')
+                print(f'{bcolors.ENDC}The meaning of {verb} is: {bcolors.WARNING}{meaning}')
+                i += 1
+            else:
+                print(f'{bcolors.FAIL}WRONG!')
+                print(f'{bcolors.ENDC}The verb is {verb}')
+                i = 0
+
+        if question == 5:
+            print(f'{bcolors.ENDC}What is the {bcolors.UNDERLINE}past pasticiple form{bcolors.ENDC} which past simple is \'{pastSimple.upper()}\'?')
+            v = input('Verb: ')
+
+            rightV = False
+
+            if v == pastParticiple:
+                rightV = True
+
+            if rightV:
+                print(f'{bcolors.OKGREEN}RIGHT!')
+                print(f'{bcolors.ENDC}The meaning of {verb} is: {bcolors.WARNING}{meaning}')
+                i += 1
+            else:
+                print(f'{bcolors.FAIL}WRONG!')
+                print(f'{bcolors.ENDC}The verb is {verb}')
+                i = 0
+
+        print(f'{bcolors.ENDC}|'+f'{bcolors.OKGREEN}#'*i+f'{bcolors.ENDC}#'*(50-i)+'|')
+
+        print(f'{bcolors.OKBLUE}='*20)
+
+        if i == 50:
+            print(f'{bcolors.OKCYAN}CONGRATULATIONS! YOU WON THE GAME!')
 
 
 try:                                                   # ----- LISTA DE VERBOS
